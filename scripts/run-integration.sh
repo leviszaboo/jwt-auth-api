f736d34afdc03ca4b0f8aa1bf694df88b477282c
Levi <leviszaboo@icloud.com>
2024-03-08 14:10:14 +0100

    test: modify integration test run file to work on custom runner

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -3,7 +3,7 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/setenv.sh
-docker-compose up -d
+docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 if [ "$#" -eq  "0" ]
