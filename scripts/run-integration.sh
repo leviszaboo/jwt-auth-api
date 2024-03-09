3a5dc9960446211ab21b8fd5a100e069c1ac0635
Levi <leviszaboo@icloud.com>
2024-03-09 11:20:33 +0100

    ci: change postgres host

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -3,9 +3,9 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/setenv.sh
-docker compose -f docker-compose.yml -f docker-compose.test.yml up -d
+docker compose -f up -d
 echo '🟡 - Testing database connection...'
-PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -p 5432 -U postgres -c 'SELECT 1;' > /dev/null
+PGPASSWORD=$POSTGRES_PASSWORD psql -h postgres -p 5432 -U postgres -c 'SELECT 1;' > /dev/null
 echo '🟢 - Database connection is ready!'
 if [ "$#" -eq  "0" ]
   then
