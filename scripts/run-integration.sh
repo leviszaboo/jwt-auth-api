032ee6f2168782740c4a0903ec89dbeaaf23bd88
Levi <leviszaboo@icloud.com>
2024-03-09 10:56:37 +0100

    ci: add host network mode to fix ci issues

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -3,11 +3,9 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/setenv.sh
-docker compose up -d
-echo '🟡 - Waiting for database to be ready...'
-$DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
+docker compose -f docker-compose.yml -f docker-compose.test.yml up -d
 echo '🟡 - Testing database connection...'
-PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -p 5532 -U postgres -c 'SELECT 1;' > /dev/null
+PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -p 5432 -U postgres -c 'SELECT 1;' > /dev/null
 echo '🟢 - Database connection is ready!'
 if [ "$#" -eq  "0" ]
   then
